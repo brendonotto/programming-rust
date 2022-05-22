5f92ff7ffd28273f582c94be3dcd519abf6285b4
Brendon Otto <brendon@pop-os.localdomain>
2022-05-22 10:27:15 -0500

    Fleshing out mandelbrot implementation

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,4 +1,5 @@
 use num::Complex;
+use std::str::FromStr;
 
 fn main() {
     println!("Hello, world!");
@@ -6,8 +7,8 @@ fn main() {
 
 /// Try to determine if `c` is in the Mandelbrot set, using at most `limit`
 /// iterations do decide.
-/// 
-/// If `c` is not a member, return `Some(i)`, where `i` is the number of 
+///
+/// If `c` is not a member, return `Some(i)`, where `i` is the number of
 /// iterations it took for `c` to leave the circle of radius 2 coentered on the
 /// origin. If `c` seems to be a member (more precisely, if we reached the
 /// iteration limit without being able to prove that `c` is not a member),
@@ -16,10 +17,95 @@ fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     let mut z = Complex { re: 0.0, im: 0.0 };
     for i in 0..limit {
         if z.norm_sqr() > 4.0 {
-            return Some(t);
+            return Some(i);
         }
         z = z * z + c;
     }
 
     None
+}
+
+/// Parse the string `s` as a coordinate pair, like `"400x600"` or `"1.0,0.5"`.
+///
+/// Specifically, `s` should have the form <left><sep><right>, where <sep> is
+/// the character given by the `separator` argument, and <left> and <right> are
+/// both strings that can be parsed by `T::from_str`. `separator` must be an
+/// ASCII character.
+///
+/// If `s` has the proper form, return `Some<(x, y)>`. If it doesn't parse
+/// correctly, return `None`.
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        // Separator not found, return None
+        None => None,
+
+        // Separator matched, extract index into closure
+        Some(index) => {
+            // Grab the values to the left of the index and attempt to call `from_str`
+            // Same for the right of the index
+            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+                // If `from_str` succeeds on both left & right of the separator, return the values
+                (Ok(l), Ok(r)) => Some((l, r)),
+                // Otherise, return None.
+                _ => None,
+            }
+        }
+    }
+}
+
+#[test]
+fn test_parse_pair() {
+    assert_eq!(parse_pair::<i32>("",        ','), None);
+    assert_eq!(parse_pair::<i32>("10,",     ','), None);
+    assert_eq!(parse_pair::<i32>(",10",     ','), None);
+    assert_eq!(parse_pair::<i32>("10,20",   ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x",    'x'), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+}
+
+/// Parse a pair of floating-point numbers separated by comma as a complex
+/// number.
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None
+    }
+}
+
+#[test]
+fn test_parse_complex() {
+    assert_eq!(parse_complex("1.25,-0.0625"),
+                            Some(Complex { re: 1.25, im: -0.0625}));
+    assert_eq!(parse_complex(",-0.0625"), None);
+}
+
+/// Given the row and column of a pixel in the output image, return the
+/// corresponding point on the complex plan.
+/// 
+/// `bounds` is a pair giving the width and height of the image in pixels.
+/// `pixel` is a (column, row) pair indiciating a particular pixel in that image.
+/// The `upper_left` and and `lower_right` parameters are points on the complex
+/// plane designating the area our image covers.
+fn pixel_to_point(bounds: (usize, usize),
+                  pixel: (usize, usize),
+                  upper_left: Complex<f64>,
+                  lower_right: Complex<f64>)
+   -> Complex<f64>
+{
+    let (width, height) = (lower_right.re - upper_left.re,
+                        upper_left.im - lower_right.im);
+    
+    Complex {
+        re: upper_left.re + pixel.0 as f64 * width  / bounds.0 as f64,
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+        // Why substraction here? pixel.1 increases as we go down,
+        // but the imaginatory component increases as we go up.
+    }
+}
+
+#[test]
+fn text_pixel_to_point() {
+    assert_eq!(pixel_to_point((100, 200), (25, 175), Complex { re: -1.0, im: 1.0}, Complex { re: 1.0, im: -1.0 }),
+               Complex { re: -0.5, im: -0.75 });
 }
